92e70091e0d466b1c812657db464954510fbdc55
nitbot <niamtamami@gmail.com>
2016-05-15 12:25:53 +0700

    cek program hari minggu
    cek program hari minggu

--- miniPC-Program/ArduinoProgram/nitbot-iotArduino/nitbot-iot/nitbot-iot.ino
+++ miniPC-Program/ArduinoProgram/nitbot-iotArduino/nitbot-iot/nitbot-iot.ino
@@ -61,6 +61,10 @@ void loop() {
   // print out the value you read:
   Serial.println(voltage);
   delay(2000);
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
 
 }
 
